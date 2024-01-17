a38e5fe275ff86a565b518d1a024b2ab5b679c8d
Denys Kosyriev <utross2241@gmail.com>
2024-01-17 22:32:29 +0200

    feature: modal forms Sign In and Sign Up

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,40 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+    // Work modal Sign In form
+
+    const modal = document.getElementsByClassName('modal-js');
+    const modalContent = document.getElementsByClassName('modal-content-js');
+    const body = document.getElementsByTagName('body')[0];
+    const signInBtn = document.getElementsByClassName('sign-in-btn-js')[0];
+    const signUpBtn = document.getElementsByClassName('sign-up-btn-js')[0];
+
+    signInBtn.addEventListener('click', () => {
+        modal[0].classList.remove('hidden');
+        modal[0].classList.add('flex');
+        body.classList.add('overflow-hidden');
+    })
+
+    signUpBtn.addEventListener('click', () => {
+        modal[1].classList.remove('hidden');
+        modal[1].classList.add('flex');
+        body.classList.add('overflow-hidden');
+    })
+
+    for (let i = 0; i < modalContent.length; i++) {
+        modal[i].addEventListener('click', (e) => {
+            const closeModal = e.composedPath().includes(modalContent[i]);
+            if (!closeModal || e.target.classList.contains('modal-icon-close')) {
+                modal[i].classList.add('hidden');
+                modal[i].classList.add('flex');
+                body.classList.remove('overflow-hidden');
+            }
+        })
+    }
+
     // Add shadow of header then scroll
 
     const header = document.getElementsByTagName('header')[0];
+
     header.classList.remove('page-scroll');
     window.addEventListener("scroll", () => {
         if (window.scrollY < 60) {
@@ -14,12 +46,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Work burger menu
 
-    const body = document.getElementsByTagName('body')[0];
     const burger = document.getElementsByClassName('burger')[0];
-    header.classList.remove('burger-active');
-    body.classList.remove('burger-active');
     burger.addEventListener('click', () => {
-        body.classList.toggle('burger-active');
+        body.classList.toggle('overflow-hidden');
+        header.classList.toggle('burger-active');
     })
 
     // Connection swiper-comments
